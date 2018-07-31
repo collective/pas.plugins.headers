82ef3ec04174435744be9660b8a4ecf49279f100
Maurits van Rees <maurits@vanrees.org>
2018-08-01 00:34:17 +0200

    Replaced variable ID with plugin_id.
    Uppercase was not nice here.

--- src/pas/plugins/headers/setuphandlers.py
+++ src/pas/plugins/headers/setuphandlers.py
@@ -23,18 +23,18 @@ def post_install(context):
     """Post install script"""
     # Setup our request header plugin.
     pas = getToolByName(context, 'acl_users')
-    ID = 'request_headers'
+    plugin_id = 'request_headers'
 
     # Create plugin if it does not exist.
-    if ID not in pas.objectIds():
+    if plugin_id not in pas.objectIds():
         from pas.plugins.headers.plugins import HeaderPlugin
         plugin = HeaderPlugin(
             title='Request Headers',
         )
-        plugin.id = ID
-        pas._setObject(ID, plugin)
-        logger.info('Created %s in acl_users.', ID)
-    plugin = getattr(pas, ID)
+        plugin.id = plugin_id
+        pas._setObject(plugin_id, plugin)
+        logger.info('Created %s in acl_users.', plugin_id)
+    plugin = getattr(pas, plugin_id)
 
     # Activate all supported interfaces for this plugin.
     activate = []
@@ -58,8 +58,8 @@ def post_install(context):
         if interface_name in ['IChallengePlugin', 'IPropertiesPlugin']:
             iface = plugins._getInterfaceFromName(interface_name)
             for obj in plugins.listPlugins(iface):
-                plugins.movePluginsUp(iface, [ID])
-            logger.info('Moved %s to top of %s.', ID, interface_name)
+                plugins.movePluginsUp(iface, [plugin_id])
+            logger.info('Moved %s to top of %s.', plugin_id, interface_name)
 
 
 def uninstall(context):
