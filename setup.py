b8d02cc17f28a1aaf3ee8c36b1f9ca7dc960a692
Jens W. Klein <jk@kleinundpartner.at>
2025-03-18 13:47:50 +0100

    amke pyroma happy

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ setup(
     package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
+    python_requires=">=3.9",
     install_requires=["Products.GenericSetup", "Products.PluggableAuthService"],
     extras_require={
         "test": [
